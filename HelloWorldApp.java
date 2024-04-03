a5e6904fc88308948802d7ff3097eafcfe410bbb
Den-k0 <denys.kotovskiy@gmail.com>
2024-04-03 20:04:37 +0300

    Додано документацію

---
+++ HelloWorldApp.java
@@ -0,0 +1,13 @@
+/**
+* The HelloWorld program implements an application that
+* simply displays "Hello World!" to the standard output.
+*
+* @author  Den Ko
+* @version 1.0
+* @since   2024-01-01
+*/
+public class HelloWorldApp {
+    public static void main(String[] args) {
+        System.out.println("Hello World!"); // Display the string.
+    }
+}
